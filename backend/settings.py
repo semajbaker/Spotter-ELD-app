74f035ab92f0e63ca1d969fd1d31a71b4a01b4ff
James Baker <bakersemaj0@gmail.com>
2025-10-17 08:02:35 +0200

    modified:   backend/settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app']
-#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+#ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 # Application definition
 
